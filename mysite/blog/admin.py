b4b9f3d4c35c67f30683dca5b150d6d4e1344e50
derekH13 <168042164+derekH13@users.noreply.github.com>
2024-11-06 09:02:16 -0300

    add exercicio

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -3,10 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'status', 'created_on']
     list_filter = ('status', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(Post, PostAdmin)
+
+class PostEvercicio(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status', )
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title', )}
+
+
+# faz aparecer um post no django admin
+admin.site.register(Post, PostEvercicio)
